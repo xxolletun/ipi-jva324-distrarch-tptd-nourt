c2b156f02fbbb29c85d9623a46abcb17e7fc99a9
Humblot Nour-Teylor <nourteylor@gmail.com>
2026-05-06 11:21:41 +0200

    Third commit

--- commande/src/main/java/com/ipi/jva324/Jva324Application.java
+++ commande/src/main/java/com/ipi/jva324/Jva324Application.java
@@ -3,6 +3,11 @@ package com.ipi.jva324;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.ipi.jva324.stock.model.ProduitEnStock;
+import com.ipi.jva324.stock.model.ReceptionDeProduit;
 
 @SpringBootApplication
 public class Jva324Application {
@@ -22,6 +27,13 @@ public class Jva324Application {
 
 		}
 
+	}
+	
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
+		return RepositoryRestConfigurer.withConfig(config -> {
+			config.exposeIdsFor(ProduitEnStock.class);
+		});
 	}
 
 }
